c1c01b26d60d3008f9e646c48cbc4aa6b5c056d7
Efekan.Gvndk <76440360+efekangvndk@users.noreply.github.com>
2024-05-11 20:35:40 +0300

    Initial Commit

---
+++ FUDA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FUDA
+//
+//  Created by Efekan Güvendik on 11.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
